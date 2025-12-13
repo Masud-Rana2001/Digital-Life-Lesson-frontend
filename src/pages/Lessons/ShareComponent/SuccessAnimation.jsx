53059211613204f4b89d7a30a8110c978977b803
Masud Rana <mdmasudrana307801@gmail.com>
2025-12-13 22:30:18 +0600

    Implement Lottie React in add Lesseon

--- src/pages/Lessons/ShareComponent/SuccessAnimation.jsx
+++ src/pages/Lessons/ShareComponent/SuccessAnimation.jsx
@@ -1,20 +1,20 @@
-// import Lottie from "lottie-react";
-// import successAnimation from "../../../assets/lottie/Success.json";
+import Lottie from "lottie-react";
+import successAnimation from "../../../assets/lottie/Success.json";
 
-// export default function SuccessAnimation({ message }) {
-//   return (
-//     <div className="
-//       fixed inset-0 z-[9999]
-//       flex flex-col items-center justify-center
-//       bg-black/40 backdrop-blur-sm
-//     ">
-//       <div className="w-72">
-//         <Lottie animationData={successAnimation} loop={false} />
-//       </div>
+export default function SuccessAnimation({ message }) {
+  return (
+    <div className="
+      fixed inset-0 z-[9999]
+      flex flex-col items-center justify-center
+      bg-black/40 backdrop-blur-sm
+    ">
+      <div className="w-72">
+        <Lottie animationData={successAnimation} loop={false} />
+      </div>
 
-//       <h2 className="mt-6 text-2xl font-bold text-white">
-//         {message || "Lesson Saved Successfully"}
-//       </h2>
-//     </div>
-//   );
-// }
+      <h2 className="mt-6 text-2xl font-bold text-white">
+        {message || "Lesson Saved Successfully"}
+      </h2>
+    </div>
+  );
+}
